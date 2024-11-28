a5c72caeff951bae0484d35b0d572e5e7a5eee31
Aschalew524 <abayasche0@gmail.com>
2024-11-28 23:03:36 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rentapp/firebase_options.dart';
+import 'package:rentapp/onboarding_page.dart';
+import 'package:rentapp/presentation/pages/car_list_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
+  
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: CarListScreen(),
+      );
+  }
+}
---
+++ lib/presentation/pages/car_list_screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+ import 'package:rentapp/data_models/Car.dart';
+import 'package:rentapp/presentation/widgets/car_card.dart';
+
+class CarListScreen  extends StatelessWidget {
+  CarListScreen({Key? key}) : super(key: key);
+  final List<Car> cars =[
+
+  Car(model: "Fortune", distance: 870, fuelCapacity: 50, pricePerHour: 20),
+    Car(model: "Fortune", distance: 870, fuelCapacity: 50, pricePerHour: 20),
+    Car(model: "Fortune", distance: 870, fuelCapacity: 50, pricePerHour: 20),
+    Car(model: "Fortune", distance: 870, fuelCapacity: 50, pricePerHour: 20),
+    Car(model: "Fortune", distance: 870, fuelCapacity: 50, pricePerHour: 20),
+    Car(model: "Fortune", distance: 870, fuelCapacity: 50, pricePerHour: 20),
+  ];
+    
+  @override
+  Widget build(BuildContext context) {
+  
+  return Scaffold(
+    appBar: AppBar(),
+    body: ListView.builder(  
+      itemCount: cars.length,
+      itemBuilder: (context, index) {
+        return CarCard(car: cars[index]);
+      },
+    ),
+    
+  );
+  
+} 
+
+
+}
