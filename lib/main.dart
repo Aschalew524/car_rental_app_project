b4181b1425ac63f6d75f3c4bea91a177c30fe321
Aschalew524 <abayasche0@gmail.com>
2024-11-30 22:12:30 +0300

    navigation added

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:rentapp/firebase_options.dart';
+import 'package:rentapp/onboarding_page.dart';
 import 'package:rentapp/presentation/pages/car_details_page.dart';
+import 'package:rentapp/presentation/pages/car_list_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,7 +20,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: CardetailsPage(),
+        home: CarListScreen(),
       );
   }
 }
